d18b631200666753495e76730d7cb5146a92dae5
余瑞银 <yuruiyin@cyou-inc.com>
2019-05-23 11:48:49 +0800

    设置图片高度最大不超过宽度的3倍

--- richeditor/src/main/java/com/yuruiyin/richeditor/config/AppConfig.java
+++ richeditor/src/main/java/com/yuruiyin/richeditor/config/AppConfig.java
@@ -10,13 +10,8 @@ package com.yuruiyin.richeditor.config;
 public interface AppConfig {
 
     /**
-     * 长图超过屏幕高度的1.5倍则显示长图标记
-     */
-    double LONG_IMAGE_RADIO = 1.5;
-
-    /**
      * 编辑器中显示出来的图片高度不会超过宽度的2倍
      */
-    double IMAGE_MAX_HEIGHT_WIDTH_RATIO = 2.0;
+    double IMAGE_MAX_HEIGHT_WIDTH_RATIO = 3.0;
 
 }
